5ddffbb4c758499cfadea42af3e2fa5b278d0f50
Samrat-14 <samratdewdrops0@gmail.com>
2022-02-08 18:24:38 +0530

    id updated to uuid

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 class Gps(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     latitude = models.DecimalField(max_digits=8, decimal_places=5)
     longitude = models.DecimalField(max_digits=8, decimal_places=5)
     timestamp = models.DateTimeField(auto_now=True)
